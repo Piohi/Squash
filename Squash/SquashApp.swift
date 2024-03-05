9717e3fbd9766b3f04177137039e9a4d0e0c9e20
Piohi <box1234567b@gmail.com>
2024-03-05 18:59:10 +0300

    Initial Commit

---
+++ Squash/SquashApp.swift
@@ -0,0 +1,32 @@
+//
+//  SquashApp.swift
+//  Squash
+//
+//  Created by Anton Godunov on 05.03.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SquashApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
